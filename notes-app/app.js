8678fd6bde0903c3d9252ca4d562706f5aed44d6
dannykrug <dannykrug@gmail.com>
2019-03-11 12:50:05 -0400

    chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,5 @@
 const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const notes = getNotes()
@@ -6,3 +7,5 @@ const notes = getNotes()
 console.log(notes)
 
 console.log(validator.isURL('krug.com'))
+
+console.log(chalk.green('Success!'))
