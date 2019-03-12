94c56e4fad702e16fbaeea85b214056a541837ef
dannykrug <dannykrug@gmail.com>
2019-03-12 10:35:17 -0400

    yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,50 @@
-const validator = require('validator')
+const yargs = require('yargs')
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+//Customize Yargs version
 
-console.log(process.argv)
+yargs.version('1.1.0')
 
-if (command === 'add') {
-  console.log('Adding note')
-} else if (command === 'remove') {
-  console.log('Removing note')
-}
+//Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function() {
+    console.log("Adding a New Note")
+  }
+})
+
+//Create remove command
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log("Removing the note")
+  }
+})
+
+//Create read command
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log("Reading a note")
+  }
+})
+
+//Create list command
+
+yargs.command({
+  command: 'list',
+  describe: 'List the notes',
+  handler: function() {
+    console.log('Listing all Notes')
+  }
+})
+
+//add, remove, read, list
+
+console.log(yargs.argv);
