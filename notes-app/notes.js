69fccb5e974383a6142f3cdcdff94ca0a38b115f
dannykrug <dannykrug@gmail.com>
2019-03-12 18:38:43 -0400

    remove function working

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 const getNotes = function () {
   return "Your notes..."
 }
@@ -21,6 +22,20 @@ const addNote = function (title, body) {
   }
 }
 
+const removeNote = function(title) {
+  const notes = loadNotes()
+  const notesToKeep = notes.filter(function(note) {
+    return note.title !== title
+  })
+
+  if(notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note Removed!'))
+    saveNotes(notesToKeep)
+  } else {
+    console.log(chalk.red.inverse('No Note Found!'))
+  }
+}
+
 const saveNotes = function (notes) {
   const dataJSON = JSON.stringify(notes)
   fs.writeFileSync('notes.json', dataJSON)
@@ -38,5 +53,6 @@ const loadNotes = function () {
 
 module.exports = {
   getNotes: getNotes,
-  addNote: addNote
+  addNote: addNote,
+  removeNote: removeNote
 }
